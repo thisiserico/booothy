70672d88f26faf6796525371f1e63d2cc933bfde
Eric Lopez <e.lopez.12491@gmail.com>
2015-06-21 12:58:53 +0200

    Implementing the Photos\GetCollection controller

--- src/App/Ui/Silex/Web/Controller/Api/Photos/GetCollection.php
+++ src/App/Ui/Silex/Web/Controller/Api/Photos/GetCollection.php
@@ -5,11 +5,15 @@ namespace App\Ui\Silex\Web\Controller\Api\Photos;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request as SilexRequest;
 use Symfony\Component\HttpFoundation\JsonResponse as SilexResponse;
+use Booothy\Photo\Application\Service\GetCompleteCollection\Request;
 
 final class GetCollection
 {
     public function __invoke(SilexRequest $silex_request, Application $app)
     {
-        return new SilexResponse(['status' => 'ok']);
+        $use_case = $app['container']->get('photo.application.service.get_complete_collection');
+        $response = $use_case(new Request);
+
+        return new SilexResponse([]);
     }
 }
