078ee054c990ac969a2560d4963ac97b4eee69ef
Eric Lopez <e.lopez.12491@gmail.com>
2015-07-15 12:32:08 +0200

    Persisting the uploaded image in a temporary directory

--- src/App/Ui/Silex/Web/Controller/Api/Photos/PostResource.php
+++ src/App/Ui/Silex/Web/Controller/Api/Photos/PostResource.php
@@ -15,13 +15,12 @@ final class PostResource
     {
         $quote         = $silex_request->get('quote');
         $uploaded_file = $silex_request->files->get('uploaded_file');
+        $mime_type     = $uploaded_file->getClientMimeType();
+        $file_path     = BASE_DIR . 'var/tmp/' . $uploaded_file->getFilename();
+        $uploaded_file->move(BASE_DIR . 'var/tmp', $uploaded_file->getFilename());
 
         $use_case = $app['container']->get(self::USE_CASE);
-        $response = $use_case(new Request(
-            $quote,
-            $uploaded_file->getClientMimeType(),
-            $uploaded_file->getRealPath()
-        ));
+        $response = $use_case(new Request($quote, $mime_type, $file_path));
 
         return new SilexResponse($response);
     }
