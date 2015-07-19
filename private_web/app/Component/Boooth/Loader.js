223ba2b5e067c6bb8a1655d65679e76a38fc9a16
Eric Lopez <e.lopez.12491@gmail.com>
2015-07-19 11:45:00 +0200

    Redirecting / to page/:page

---
+++ private_web/app/Component/Boooth/Loader.js
@@ -0,0 +1,18 @@
+var Link         = require('react-router').Link;
+var PhotoList    = require('../PhotoStream/PhotoList.js');
+var React        = require('react');
+var RouteHandler = require('react-router').RouteHandler;
+
+var Loader = React.createClass({
+    render : function() {
+        return (
+            <div>
+                <Link to="boooth" params={{ page : this.props.params.page }}>Boooth!</Link>
+                <RouteHandler/>
+                <PhotoList />
+            </div>
+        );
+    }
+});
+
+module.exports = Loader;
--- private_web/app/Component/Booothy/App.js
+++ private_web/app/Component/Booothy/App.js
@@ -1,16 +1,10 @@
-var Link         = require('react-router').Link;
-var PhotoList    = require('../PhotoStream/PhotoList.js');
 var React        = require('react');
 var RouteHandler = require('react-router').RouteHandler;
 
 var App = React.createClass({
     render : function() {
         return (
-            <div>
-                <Link to="boooth">Boooth!</Link>
-                <RouteHandler/>
-                <PhotoList />
-            </div>
+            <RouteHandler/>
         );
     }
 });
