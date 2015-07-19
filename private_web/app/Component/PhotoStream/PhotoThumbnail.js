772e897ea2eb897d3e03095122afa2ee18106152
Eric Lopez <e.lopez.12491@gmail.com>
2015-07-19 23:23:44 +0200

    Adding an opaque layer over the boooths to display the quote

--- private_web/app/Component/PhotoStream/PhotoThumbnail.js
+++ private_web/app/Component/PhotoStream/PhotoThumbnail.js
@@ -3,7 +3,7 @@ var React = require('react');
 var PhotoThumbnail = React.createClass({
     render: function() {
         return (
-            <div className="boooth_thumbnail">
+            <div className="boooth_thumbnail" data-content={this.props.photo['quote']}>
                 <img src={this.props.photo['upload']['download_url']} />
             </div>
         );
