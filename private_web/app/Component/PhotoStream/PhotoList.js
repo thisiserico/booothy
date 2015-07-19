2ea58d0c5f2389add26ce1a245143c5e44519361
Eric Lopez <e.lopez.12491@gmail.com>
2015-07-19 18:07:13 +0200

    Loading new boooth pages when reaching the page bottom

--- private_web/app/Component/PhotoStream/PhotoList.js
+++ private_web/app/Component/PhotoStream/PhotoList.js
@@ -4,32 +4,86 @@ var PhotoStore   = require('../../Store/PhotoStore');
 var React        = require('react');
 
 var PhotoList = React.createClass({
-    getState: function () {
+    getState : function () {
         return {
-            all_photos       : PhotoStore.getCollection(),
-            loading_new_set  : PhotoStore.newSetBeingLoaded(),
-            uploading_boooth : PhotoStore.booothBeingUploaded()
+            all_photos                : PhotoStore.getCollection(),
+            loading_new_set           : PhotoStore.newSetBeingLoaded(),
+            uploading_boooth          : PhotoStore.booothBeingUploaded(),
+            complete_catalogue_loaded : PhotoStore.completeCatalogueLoaded(),
+            current_page              : this.state
+                ? this.state.current_page
+                : parseInt(this.props.page),
+            loaded_pages              : this.state
+                ? this.state.loaded_pages
+                : []
         };
     },
 
-    getInitialState: function () {
+    getInitialState : function () {
         return this.getState();
     },
 
-    componentDidMount: function () {
+    componentDidMount : function () {
         PhotoStore.addChangeListener(this._onChange);
         PhotosClient.getCollection(this.props.page);
+
+        this.addScrollListener();
     },
 
-    componentWillUnmount: function () {
+    componentWillUnmount : function () {
         PhotoStore.removeChangeListener(this._onChange);
+        this.removeScrollListener();
     },
 
-    _onChange: function () {
+    addScrollListener : function () {
+        window.addEventListener('scroll', this.scrollListener);
+        window.addEventListener('resize', this.scrollListener);
+    },
+
+    removeScrollListener : function () {
+        window.removeEventListener('scroll', this.scrollListener);
+        window.removeEventListener('resize', this.scrollListener);
+    },
+
+    scrollListener : function () {
+        this.removeScrollListener();
+
+        var scrolled_height = window.pageYOffset;
+        var page_height     = window.innerHeight;
+        var body_height     = window.document.body.offsetHeight;
+        var bottom_reached  = (scrolled_height + page_height) >= body_height;
+
+        if (bottom_reached && this.shouldLoadNextPage()) {
+            var next_page = this.state.current_page + 1;
+            PhotosClient.getCollection(next_page);
+
+            var loaded_pages = this.state.loaded_pages;
+            loaded_pages.push(this.state.current_page);
+
+            this.setState({
+                current_page : next_page,
+                loaded_pages : loaded_pages
+            });
+        }
+
+        this.addScrollListener();
+    },
+
+    shouldLoadNextPage : function () {
+        var next_page = this.state.current_page + 1;
+
+        return (
+            this.state.loaded_pages.indexOf(next_page) === -1
+            && !this.state.loading_new_set
+            && !this.state.complete_catalogue_loaded
+        );
+    },
+
+    _onChange : function () {
         this.setState(this.getState());
     },
 
-    render: function () {
+    render : function () {
         var photos = [];
 
         this.state.all_photos.map(function (photo) {
@@ -41,6 +95,7 @@ var PhotoList = React.createClass({
                 <pre>{this.state.loading_new_set ? 'Loading' : ''}</pre>
                 <pre>{this.state.uploading_boooth ? 'Uploading' : ''}</pre>
                 {photos}
+                <pre>{this.state.complete_catalogue_loaded ? 'No more boooths to be shown!' : ''}</pre>
             </div>
         );
     }
