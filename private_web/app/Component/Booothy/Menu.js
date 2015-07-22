a87211f186df7852a7fe77519339f86e2f961fea
Eric Lopez <e.lopez.12491@gmail.com>
2015-07-22 14:57:45 +0200

    Designing the menu buttons

--- private_web/app/Component/Booothy/Menu.js
+++ private_web/app/Component/Booothy/Menu.js
@@ -3,10 +3,34 @@ var React        = require('react');
 var RouteHandler = require('react-router').RouteHandler;
 
 var Menu = React.createClass({
+    getInitialState : function () {
+        return { menu_hover : false };
+    },
+
+    _menuMouseOver : function () {
+        if (this.state.menu_hover) return;
+
+        this.setState({ menu_hover : true });
+    },
+
+    _menuMouseOut : function () {
+        if (!this.state.menu_hover) return;
+
+        this.setState({ menu_hover : false });
+    },
+
     render : function() {
         return (
-            <div>
-                <Link to="boooth">Boooth!</Link>
+            <div className="menu" onMouseOver={this._menuMouseOver} onMouseOut={this._menuMouseOut}>
+                <Link to="boooth" style={{ display : this.state.menu_hover ? 'block' : 'none' }}><i className="fa fa-sign-out" /></Link>
+                <Link to="boooth" style={{ display : this.state.menu_hover ? 'block' : 'none' }}><i className="fa fa-cog" /></Link>
+
+                <Link to="boooth">
+                    {this.state.menu_hover
+                        ? <i className="fa fa-camera" />
+                        : <i className="fa fa-plus" />
+                    }
+                </Link>
             </div>
         );
     }
