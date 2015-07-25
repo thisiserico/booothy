36285259841bd9fa41af82fd76ecaa823a03330d
Eric Lopez <e.lopez.12491@gmail.com>
2015-07-25 11:51:18 +0200

    Writting the production log

--- src/App/Ui/Silex/Web/Public/index.php
+++ src/App/Ui/Silex/Web/Public/index.php
@@ -2,11 +2,17 @@
 
 ini_set('display_errors', 0);
 
+use Silex\Provider\MonologServiceProvider;
+
 define('ENV', 'prod');
 
 require_once BASE_DIR . 'vendor/autoload.php';
 
 $app = require __DIR__.'/../Application.php';
+
+$app->register(new MonologServiceProvider, [
+    'monolog.logfile' => $app['container']->getParameter('folder.logs') . 'silex.log',
+]);
 
 $app['twig.path']    = [__DIR__.'/../../../Templates'];
 $app['twig.options'] = ['cache' => $app['container']->getParameter('folder.cache') . 'twig'];
