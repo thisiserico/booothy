b2e8ac19d4ef0668395d8101478fc1a66fbdfa74
Eric Lopez <e.lopez.12491@gmail.com>
2015-07-25 10:13:55 +0200

    Adding the avatar to the GET /users/ end points

--- src/Booothy/User/Domain/Model/User.php
+++ src/Booothy/User/Domain/Model/User.php
@@ -6,6 +6,8 @@ use Booothy\User\Domain\Model\ValueObject\Email;
 
 final class User
 {
+    const AVATAR_PATTERN = 'http://www.gravatar.com/avatar/';
+
     private $email;
 
     public function __construct(Email $an_email)
@@ -16,5 +18,10 @@ final class User
     public function email()
     {
         return $this->email;
+    }
+
+    public function avatar()
+    {
+        return self::AVATAR_PATTERN . md5($this->email->value());
     }
 }
