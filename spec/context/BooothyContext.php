59dc14eec2bd1e546e7fe55bcccef85137f7524a
Eric Lopez <e.lopez.12491@gmail.com>
2015-08-05 20:45:26 +0200

    Creating a new testing environment to define custom services

--- spec/context/BooothyContext.php
+++ spec/context/BooothyContext.php
@@ -8,30 +8,35 @@ use Behat\Gherkin\Node\PyStringNode;
 
 class BooothyContext implements Context
 {
-    /**
-     * @Given /^the list with the allowed users:$/
-     */
+    protected static $service_container;
+
+    /** @BeforeSuite */
+    public static function loadServiceContainer()
+    {
+        $file_name = 'definition_test.php';
+        $file_path = 'src/App/DependencyInjection/Services/' . $file_name;
+        require_once $file_path;
+
+        $class_name              = 'TestServiceContainer';
+        self::$service_container = new $class_name;
+    }
+
+    /** @Given /^the list with the allowed users:$/ */
     public function listWithTheAllowedUsers(TableNode $allowed_users)
     {
     }
 
-    /**
-     * @When /^I send a ([A-Z]+) request to (.+)$/
-     */
+    /** @When /^I send a ([A-Z]+) request to (.+)$/ */
     public function sendHttpRequestToUri($http_method, $uri)
     {
     }
 
-    /**
-     * @Then /^I should obtain a (\d+) status code$/
-     */
+    /** @Then /^I should obtain a (\d+) status code$/ */
     public function shouldObtainTheStatusCode($http_status_code)
     {
     }
 
-    /**
-     * @Then /^the response should be$/
-     */
+    /** @Then /^the response should be$/ */
     public function theResponseShouldBe(PyStringNode $expected_response)
     {
     }
